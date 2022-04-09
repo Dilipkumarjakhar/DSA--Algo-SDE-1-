310462f14a7f8407fcdc0c16f2913b639f61e332
DILIP KUMAR JAKHAR <jakhardilip10@gmail.com>
2022-04-09 16:26:49 +0530

    Update pre.js

--- pre.js
+++ pre.js
@@ -1,23 +1,33 @@
 
-const demo=(arr,func)=>{
-    while(arr.length-1>0 && !func(arr[arr.length-1]))
-    arr=arr.slice(0,-1);
-    return arr;
-}
+// const demo=(arr,func)=>{
+//     while(arr.length-1>0 && !func(arr[arr.length-1]))
+//     arr=arr.slice(0,-1);
+//     return arr;
+// }
 
-console.log(demo([2,3,4,5],n=>n<4))
+// console.log(demo([2,3,4,5],n=>n<4))
 
-const user={
-    email:'dili@1.com',
+// const user={
+//     email:'dili@1.com',
 
-    updateEmail:email=>{
-        this.email=email
-    }
-}
-user.updateEmail('newemail12@demo.com');
-console.log(user.email)
+//     updateEmail:email=>{
+//         this.email=email
+//     }
+// }
+// user.updateEmail('newemail12@demo.com');
+// console.log(user.email)
 
-let a=[1,2,3]
-for(const i=0;i<a.length;i++){
-    console.log(a[i])
-}
+// let a=[1,2,3]
+// for(const i=0;i<a.length;i++){
+//     console.log(a[i])
+// }
+
+
+// for(let i=0;i<5;i++){
+
+// }
+// console.log(i);
+// let a=5;
+// let b="6"
+// let s=a+b;
+// console.log(typeof s,a+b);
